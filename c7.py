66099e6a43252e5d259529db7c0c1752d44091f9
bobbydurrett <bobby@bobbydurrettdba.com>
2021-04-09 13:36:27 -0700

    unicode and latex on Linux console

--- c7.py
+++ c7.py
@@ -8,3 +8,11 @@ init_printing(use_unicode=False)
 
 pprint(Integral(sqrt(1/x), x))
 
+init_printing(use_unicode=True)
+
+pprint(Integral(sqrt(1/x), x))
+
+from lprint import lprint
+
+lprint(Integral(sqrt(1/x), x))
+
