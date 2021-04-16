3c480dce3593239c85a082e407a5a1011addb1a1
bobbydurrett <bobby@bobbydurrettdba.com>
2021-04-16 16:08:09 -0700

    finished matrices

--- c12.py
+++ c12.py
@@ -54,8 +54,119 @@ spprint(M**-1)
 # sympy.matrices.common.NonInvertibleMatrixError: Matrix det == 0; not invertible.
 #spprint(N**-1)
 
+# transpose
+
+M = Matrix([[1, 2, 3], [4, 5, 6]])
+spprint(M)
+
+spprint(M.T)
+
+# constructors
+
+spprint(eye(3))
+
+spprint(eye(4))
+
+spprint(zeros(2, 3))
+
+spprint(ones(3, 2))
+
+spprint(diag(1, 2, 3))
+
+spprint(diag(-1, ones(2, 2), Matrix([5, 7, 5])))
+
+# advanced
+
+M = Matrix([[1, 0, 1], [2, -1, 3], [4, 3, 2]])
+spprint(M)
+
+spprint(M.det())
+
+M = Matrix([[1, 0, 1, 3], [2, 3, 4, 7], [-1, -3, -3, -4]])
+spprint(M)
+
+spprint(M.rref())
+
+M = Matrix([[1, 2, 3, 0, 0], [4, 10, 0, 0, 1]])
+
+spprint(M)
+
+spprint(M.nullspace())
+
+M = Matrix([[1, 1, 2], [2 ,1 , 3], [3 , 1, 4]])
+
+spprint(M)
+
+spprint(M.columnspace())
+
+M = Matrix([[3, -2,  4, -2], [5,  3, -3, -2], [5, -2,  2, -2], [5, -2, -3,  3]])
+
+spprint(M)
+
+spprint(M.eigenvals())
+
+spprint(M.eigenvects())
+
+# diagonalize
+
+spprint(M)
+
+P, D = M.diagonalize()
+
+spprint(P)
+
+spprint(D)
+
+spprint(P*D*P**-1)
+
+print(P*D*P**-1 == M)
+
+lamda = symbols('lamda')
+
+p = M.charpoly(lamda)
+
+spprint(p)
+
+spprint(factor(p.as_expr()))
+
+# zero testing
+
+from sympy import *
+q = Symbol("q", positive = True)
+m = Matrix([
+[-2*cosh(q/3),      exp(-q),            1],
+[      exp(q), -2*cosh(q/3),            1],
+[           1,            1, -2*cosh(q/3)]])
+
+spprint(m.nullspace())
 
 
+import warnings
+def my_iszero(x):
+    try:
+        result = x.is_zero
+    except AttributeError:
+        result = None
+
+    # Warnings if evaluated into None
+    if result is None:
+        warnings.warn("Zero testing of {} evaluated into None".format(x))
+    return result
+
+spprint(m.nullspace(iszerofunc=my_iszero))
+
+def my_iszero(x):
+    try:
+        result = x.rewrite(exp).simplify().is_zero
+    except AttributeError:
+        result = None
+
+    # Warnings if evaluated into None
+    if result is None:
+        warnings.warn("Zero testing of {} evaluated into None".format(x))
+    return result
+
+spprint(m.nullspace(iszerofunc=my_iszero))
 
 
 
